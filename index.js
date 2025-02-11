12ec6622061625134151ee68db32562388a54536
kenco <75061562+Yeom-Seung-Ah@users.noreply.github.com>
2025-02-12 01:00:49 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+  document.getElementById("loginModal").style.display = "block";
+});
+
+document.querySelector(".close").addEventListener("click", function () {
+  document.getElementById("loginModal").style.display = "none";
+});
+window.addEventListener("click", function (event) {
+  if (event.target == document.getElementById("loginModal")) {
+    document.getElementById("loginModal").style.display = "none";
+  }
+});
